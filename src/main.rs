fa1657518866cc21ea1af7ff4e699c4aaeb69c9e
tristanhormann <triddel02@gmx.de>
2023-10-12 15:02:25 +0200

    main aufgeräumt

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,5 @@ mod tests;
 use crate::rsa::math_functions::number_theory::fast_exponentiation;
 
 fn main() {
-    let base = ubig!(561563);
-    let exponent = ubig!(1300);
-    let modul = ubig!(564);
-    let start = Instant::now();
-    println!("{}", fast_exponentiation(&base, &exponent, &modul));
-    println!("{}", start.elapsed().as_nanos());
+
 }
