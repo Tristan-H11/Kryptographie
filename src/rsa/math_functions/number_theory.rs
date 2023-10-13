6688349c4789ebee0083eca92d6126094b84c98c
Simon Ganteföhr <simon.gantefoehr@edu.fhdw.de>
2023-10-13 12:41:19 +0200

    Modulo_inverse (erweiterter euclidischer algirthmus)

--- src/rsa/math_functions/number_theory.rs
+++ src/rsa/math_functions/number_theory.rs
@@ -42,7 +42,31 @@ pub fn fast_exponentiation(base: &UBig, exponent: &UBig, modul: &UBig) -> UBig {
     };
 }
 
-pub fn expanded_euclidean_algorithm() {}
+///berechnet das inverse Element in einem Restklassenring
+///also zb. Das inverse zu 5 im Modulo 11 ist 9, da (5*9)%11 = 1 (neutrales Element der Multiplikation)
+///
+/// Faktoren im Array können negativ sein (deshalb i128) müssen dann schauen wegen bigInt
+pub fn modulo_inverse(n: i128, modul: i128)-> i128 {
+    let xy = [1, 0, 1];
+    //berechnet aus letzten faktor den inverse
+    return (modul + extended_euclidean_algorithm(modul, n, xy))%modul;
+}
+fn extended_euclidean_algorithm(n: i128, modul: i128, mut xy: [i128; 3]) -> i128 {
+    xy.rotate_left(1);
+    //println!("{},{},{} n= {} m = {}",xy[0],xy[1],xy[2],n,modul); //debug
+    if modul == 0 {
+        //check ob inverse existiert sonst error (durch 0 teilen XDDD ka wie error in rust geht)
+        if n!=1 {
+            1/modul;
+        }
+        return xy[0];
+    }else {
+        //berechnet faktoren und speichert in einem rotierendem Array
+        let div: i128 = n/modul;
+        xy[2]=xy[0]-(div*xy[1]);
+        return extended_euclidean_algorithm(modul, n%modul,xy);
+    }
+}
 
 /// Führt den Miller-Rabin-Primzahltest auf `n` durch `repeats` Runden aus.
 ///
