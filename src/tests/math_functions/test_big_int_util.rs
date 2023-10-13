03692be6a1570421a048f8e9e0f0717738dce870
tristanhormann <triddel02@gmx.de>
2023-10-13 14:23:28 +0200

    Increment und Decrement für UBig in big_int_util.rs ergänzt.

--- src/tests/math_functions/test_big_int_util.rs
+++ src/tests/math_functions/test_big_int_util.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use crate::rsa::math_functions::big_int_util::{decrement, divides, increment, is_even, is_one, is_uneven, is_zero, not_divides, random_in_range};
     use crate::rsa::math_functions::big_int_util::{
         divides, is_even, is_one, is_uneven, is_zero, not_divides,
     };
@@ -41,5 +42,24 @@ mod tests {
         assert_eq!(not_divides(&ubig!(4), &ubig!(8)), false);
         assert_eq!(not_divides(&ubig!(1), &ubig!(89893457)), false);
         assert_eq!(not_divides(&ubig!(134505), &ubig!(89893457)), true);
+    }
+
+    #[test]
+    fn test_increment() {
+        assert_eq!(increment(&ubig!(3)), ubig!(4));
+        assert_eq!(increment(&ubig!(0)), ubig!(1));
+    }
+
+    #[test]
+    fn test_decrement() {
+        assert_eq!(decrement(&ubig!(3)), ubig!(2));
+        assert_eq!(decrement(&ubig!(1)), ubig!(0));
+    }
+
+    #[test]
+    fn test_random_number_in_range() {
+        let high_num = ubig!(3453).pow(564);
+        let random = random_in_range(&high_num);
+        assert!(random <= high_num && random >= ubig!(2))
     }
 }
