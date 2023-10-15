2b51ad7550d9e5d3e8de688d9007fa1fbb638f2f
Tristan-H11 <triddel02@gmx.de>
2023-10-16 00:12:18 +0200

    encryption_service.rs Trait erstellt.

---
+++ src/encryption/encryption_service.rs
@@ -0,0 +1,29 @@
+/// Ein Trait, gegen welchen die kryptographische Operationen aufrufen werden können.
+pub trait EncryptionService {
+
+    ///
+    /// Verschlüsselt einen übergebenen String.
+    ///
+    fn encrypt(&self) -> String;
+
+    ///
+    /// Entschlüsselt einen übergebenen String.
+    ///
+    fn decrypt(&self) -> String;
+
+    ///
+    /// Signiert einen übergebenen String.
+    ///
+    fn sign(&self) -> String;
+
+    ///
+    /// Überprüft die Signatur eines übergebenen Strings.
+    ///
+    fn verify(&self) -> String;
+
+    ///
+    /// Erstellt ein Schlüsselpaar.
+    ///
+    fn generate_keypair(&self) -> (String, String);
+}
+
