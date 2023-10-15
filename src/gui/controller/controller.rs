c06d3482ada5cf63336b91d8b5f4d7db1cd10be3
Tristan-H11 <triddel02@gmx.de>
2023-10-16 01:11:15 +0200

    Testweise das Rotationschiffre in die GUI eingebaut.

--- src/gui/controller/controller.rs
+++ src/gui/controller/controller.rs
@@ -1,6 +1,10 @@
 use crate::gui::controller::commands::*;
-use crate::gui::model::model::{AliceModel, AppState, BobModel, View};
+use crate::gui::model::model::{ AppState, View};
 use druid::{Env, Event, EventCtx};
+use crate::encryption::encryption_service::EncryptionService;
+use crate::encryption::keygen_service::KeyGenService;
+use crate::encryption::rotate::rot_encryption_service::RotEncryptionService;
+use crate::encryption::rotate::rot_keygen_service::RotKeygenService;
 
 pub struct AppController;
 
@@ -52,19 +56,19 @@ impl AppController {
     fn handle_alice_event(&mut self, event: &Event, app_state: &mut AppState, _env: &Env) {
         match event {
             Event::Command(cmd) if cmd.is(ENCRYPT) => {
-                self.encrypt_alice(&mut app_state.alice);
+                self.encrypt_alice(app_state);
             }
             Event::Command(cmd) if cmd.is(SIGN) => {
-                self.sign_alice(&mut app_state.alice);
+                self.sign_alice(app_state);
             }
             Event::Command(cmd) if cmd.is(DECRYPT) => {
-                self.decrypt_alice(&mut app_state.alice);
+                self.decrypt_alice(app_state);
             }
             Event::Command(cmd) if cmd.is(SEND_MESSAGE) => {
-                self.send_message_alice(&mut app_state.alice, &mut app_state.bob);
+                self.send_message_alice(app_state);
             }
             Event::Command(cmd) if cmd.is(CLEAR) => {
-                self.clear_alice(&mut app_state.alice);
+                self.clear_alice(app_state);
             }
             Event::Command(cmd) if cmd.is(SWITCH_TO_HAUPTMENU) => {
                 app_state.current_view = View::HauptMenu;
@@ -76,19 +80,19 @@ impl AppController {
     fn handle_bob_event(&mut self, event: &Event, app_state: &mut AppState, _env: &Env) {
         match event {
             Event::Command(cmd) if cmd.is(ENCRYPT) => {
-                self.encrypt_bob(&mut app_state.bob);
+                self.encrypt_bob(app_state);
             }
             Event::Command(cmd) if cmd.is(SIGN) => {
-                self.sign_bob(&mut app_state.bob);
+                self.sign_bob(app_state);
             }
             Event::Command(cmd) if cmd.is(DECRYPT) => {
-                self.decrypt_bob(&mut app_state.bob);
+                self.decrypt_bob(app_state);
             }
             Event::Command(cmd) if cmd.is(SEND_MESSAGE) => {
-                self.send_message_bob(&mut app_state.bob, &mut app_state.alice);
+                self.send_message_bob(app_state);
             }
             Event::Command(cmd) if cmd.is(CLEAR) => {
-                self.clear_bob(&mut app_state.bob);
+                self.clear_bob(app_state);
             }
             Event::Command(cmd) if cmd.is(SWITCH_TO_HAUPTMENU) => {
                 app_state.current_view = View::HauptMenu;
@@ -100,55 +104,88 @@ impl AppController {
     //Allgemeine Funktion
     fn calculate_public_key(&mut self, app_state: &mut AppState) {
         // TODO: Implementiere die Logik zur Berechnung der öffentlichen Schlüssel für Alice und Bob
-        app_state.haupt_menu.public_key_alice = "1234567890".to_string();
-        app_state.haupt_menu.public_key_bob = "0987654321".to_string();
+        // Das ist erstmal nur eine Dummy-Implementierung mittels Rot-Chiffre
+
+        // Alice
+        let rot_keygen_service_alice = RotKeygenService::new(
+            app_state.haupt_menu.eingabe_p1.clone());
+        let (public_key_alice, private_key_alice) = rot_keygen_service_alice.generate_keypair();
+
+        app_state.haupt_menu.public_key_alice = public_key_alice.clone();
+        app_state.alice.anzeige_geheimer_schluessel = private_key_alice.clone();
+
+        // Bob
+        let rot_keygen_service_bob = RotKeygenService::new(
+            app_state.haupt_menu.eingabe_p2.clone());
+        let (public_key_bob, private_key_bob) = rot_keygen_service_bob.generate_keypair();
+
+        app_state.haupt_menu.public_key_bob = public_key_bob.clone();
+        app_state.bob.anzeige_geheimer_schluessel = private_key_bob.clone();
     }
 
     //Alice Funktion
-    fn encrypt_alice(&mut self, alice: &mut AliceModel) {
-        alice.eingabe_klartext = String::from("Unyyb Obo");
-        // todo -- Logik für Verschlüsselung für Alice
+    fn encrypt_alice(&mut self, app_state: &mut AppState) {
+        let public_key = app_state.haupt_menu.public_key_alice.parse::<u8>().unwrap();
+        let klartext = app_state.alice.eingabe_klartext.clone();
+        let service = RotEncryptionService::new(public_key);
+
+        let encrypted = service.encrypt(&klartext);
+        app_state.alice.eingabe_klartext = encrypted;
     }
-    fn sign_alice(&mut self, alice: &mut AliceModel) {
-        alice.eingabe_klartext = format!("{} Signatur", alice.eingabe_klartext);
+
+    fn sign_alice(&mut self, app_state: &mut AppState) {
         // todo -- Logik für Signierung für Alice
+        println!("Signatur Alice");
     }
-    fn decrypt_alice(&mut self, alice: &mut AliceModel) {
-        alice.eingabe_klartext = String::from("Hallo Bob");
-        // todo -- Logik für Entschlüsselung für Alice
+    fn decrypt_alice(&mut self, app_state: &mut AppState) {
+        let private_key = app_state.alice.anzeige_geheimer_schluessel.parse::<u8>().unwrap();
+        let ciphertext = app_state.alice.eingabe_klartext.clone();
+        let service = RotEncryptionService::new(private_key);
+
+        let decrypted = service.decrypt(&ciphertext);
+        app_state.alice.eingabe_klartext = decrypted;
     }
-    fn send_message_alice(&mut self, alice: &mut AliceModel, bob: &mut BobModel) {
-        let message = &alice.eingabe_klartext;
-        bob.eingabe_klartext = message.clone();
-        alice.eingabe_klartext = String::new();
+    fn send_message_alice(&mut self, app_state: &mut AppState) {
+        let message = &app_state.alice.eingabe_klartext;
+        app_state.bob.eingabe_klartext = message.clone();
+        self.clear_alice(app_state);
         // todo -- Logik zum Senden der Nachricht für Alice
     }
-    fn clear_alice(&mut self, alice: &mut AliceModel) {
-        alice.eingabe_klartext = String::new();
+    fn clear_alice(&mut self, app_state: &mut AppState) {
+        app_state.alice.eingabe_klartext = String::new();
         // todo -- Logik zum Zurücksetzen der Eingabefelder und Labels für Alice
     }
 
     //Bob Funktion
-    fn encrypt_bob(&mut self, bob: &mut BobModel) {
-        bob.eingabe_klartext = String::from("Unyyb Nyvpr");
+    fn encrypt_bob(&mut self, app_state: &mut AppState) {
+        let public_key = app_state.haupt_menu.public_key_bob.parse::<u8>().unwrap();
+        let klartext = app_state.bob.eingabe_klartext.clone();
+        let service = RotEncryptionService::new(public_key);
+
+        let encrypted = service.encrypt(&klartext);
+        app_state.bob.eingabe_klartext = encrypted;
         // todo -- Logik für Verschlüsselung für Bob
     }
-    fn sign_bob(&mut self, bob: &mut BobModel) {
-        bob.eingabe_klartext = format!("{} Signatur", bob.eingabe_klartext);
-        // todo -- Logik für Signierung für Bob
+    fn sign_bob(&mut self, app_state: &mut AppState) {
+        // todo -- Logik für Signierung für Alice
+        println!("Signatur Bob");
     }
-    fn decrypt_bob(&mut self, bob: &mut BobModel) {
-        bob.eingabe_klartext = String::from("Hallo Alice");
-        // todo -- Logik für Entschlüsselung für Bob
+    fn decrypt_bob(&mut self, app_state: &mut AppState) {
+        let private_key = app_state.bob.anzeige_geheimer_schluessel.parse::<u8>().unwrap();
+        let ciphertext = app_state.bob.eingabe_klartext.clone();
+        let service = RotEncryptionService::new(private_key);
+
+        let decrypted = service.decrypt(&ciphertext);
+        app_state.bob.eingabe_klartext = decrypted;
     }
-    fn send_message_bob(&mut self, bob: &mut BobModel, alice: &mut AliceModel) {
-        let message = &bob.eingabe_klartext;
-        alice.eingabe_klartext = message.clone();
-        bob.eingabe_klartext = String::new();
+    fn send_message_bob(&mut self, app_state: &mut AppState) {
+        let message = &app_state.bob.eingabe_klartext;
+        app_state.alice.eingabe_klartext = message.clone();
+        self.clear_bob(app_state);
         // todo -- Logik zum Senden der Nachricht für Bob
     }
-    fn clear_bob(&mut self, bob: &mut BobModel) {
-        bob.eingabe_klartext = String::new();
+    fn clear_bob(&mut self, app_state: &mut AppState) {
+        app_state.bob.eingabe_klartext = String::new();
         // todo -- Logik zum Zurücksetzen der Eingabefelder und Labels für Bob
     }
 }
