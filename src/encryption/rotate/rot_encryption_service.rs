2a855c6aaa2806bbe23d442a67641fe835487834
Tristan-H11 <triddel02@gmx.de>
2023-10-16 00:12:29 +0200

    Für Dummy Daten ein Rot_Chiffre erstellt.

---
+++ src/encryption/rotate/rot_encryption_service.rs
@@ -0,0 +1,60 @@
+use crate::encryption::encryption_service::EncryptionService;
+
+/// Eine Implementierung des EncryptionService-Traits, welches
+/// simple Dummy Daten mit dem Rotate Chiffre verwendet.
+struct RotEncryptionService {
+    message: String,
+    rotation: u8
+}
+impl RotEncryptionService {
+    ///
+    /// Erstellt eine neue Instanz des RotEncryptionService.
+    ///
+    /// # Argumente
+    ///
+    /// * `input` - Der zu verschlüsselnde String.
+    /// * `rotation` - Die Anzahl der zu verschiebenden Stellen.
+    ///
+    pub fn new(message: String, rotation: u8) -> RotEncryptionService {
+        RotEncryptionService { message, rotation }
+    }
+}
+
+impl EncryptionService for RotEncryptionService {
+    fn encrypt(&self) -> String {
+        rotate_forward(&self.message, &self.rotation)
+    }
+
+    fn decrypt(&self) -> String {
+        rotate_backward(&self.message, &self.rotation)
+    }
+
+    fn sign(&self) -> String {
+        format!("{} -- signiert!", &self.message)
+    }
+
+    fn verify(&self) -> String {
+        panic!("Nicht implementiert!")
+    }
+}
+
+fn rotate_forward(text: &str, shift: &u8) -> String {
+    text.chars()
+        .map(|c| match c {
+            'a'..='z' => ((c as u8 - b'a' + shift) % 26 + b'a') as char,
+            'A'..='Z' => ((c as u8 - b'A' + shift) % 26 + b'A') as char,
+            _ => c,
+        })
+        .collect()
+}
+
+fn rotate_backward(text: &str, shift: &u8) -> String {
+    text.chars()
+        .map(|c| match c {
+            'a'..='z' => ((c as u8 - b'a' - shift) % 26 + b'a') as char,
+            'A'..='Z' => ((c as u8 - b'A' - shift) % 26 + b'A') as char,
+            _ => c,
+        })
+        .collect()
+}
+
