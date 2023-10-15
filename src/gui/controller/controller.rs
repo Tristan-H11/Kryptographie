f740471e0b58f5ba7cb3c9476a4f4e4c31108af1
Tristan-H11 <triddel02@gmx.de>
2023-10-15 19:58:40 +0200

    Cargo fix

--- src/gui/controller/controller.rs
+++ src/gui/controller/controller.rs
@@ -1,9 +1,4 @@
-use crate::gui::gui::CALCULATE_PUBLIC_KEY;
-use crate::gui::gui::{AppController, UPDATE_PUBLIC_KEY};
-use crate::gui::gui::{CLEAR, DECRYPT, ENCRYPT, SEND_MESSAGE, SIGN};
-use crate::gui::model::model::{AliceModel, AppState, BobModel, HauptMenuModel, View};
-use crate::rsa;
-use druid::{Env, Event};
+use crate::gui::model::model::{AliceModel, BobModel, HauptMenuModel};
 
 pub struct Controller;
 
@@ -15,41 +10,41 @@ impl Controller {
         haupt_menu.public_key_bob = "0987654321".to_string();
     }
 
-    pub(crate) fn update_public_key(haupt_menu: &mut HauptMenuModel) {
+    pub(crate) fn update_public_key(_haupt_menu: &mut HauptMenuModel) {
         // todo -- Logik zum Aktualisieren des öffentlichen Schlüssels
     }
 
     //Alice Funktion
-    pub(crate) fn encrypt_alice(alice: &mut AliceModel) {
+    pub(crate) fn encrypt_alice(_alice: &mut AliceModel) {
         // todo -- Logik für Verschlüsselung für Alice
     }
-    pub(crate) fn sign_alice(alice: &mut AliceModel) {
+    pub(crate) fn sign_alice(_alice: &mut AliceModel) {
         // todo -- Logik für Signierung für Alice
     }
-    pub(crate) fn decrypt_alice(alice: &mut AliceModel) {
+    pub(crate) fn decrypt_alice(_alice: &mut AliceModel) {
         // todo -- Logik für Entschlüsselung für Alice
     }
-    pub(crate) fn send_message_alice(alice: &mut AliceModel) {
+    pub(crate) fn send_message_alice(_alice: &mut AliceModel) {
         // todo -- Logik zum Senden der Nachricht für Alice
     }
-    pub(crate) fn clear_alice(alice: &mut AliceModel) {
+    pub(crate) fn clear_alice(_alice: &mut AliceModel) {
         // todo -- Logik zum Zurücksetzen der Eingabefelder und Labels für Alice
     }
 
     //Bob Funktion
-    pub(crate) fn encrypt_bob(bob: &mut BobModel) {
+    pub(crate) fn encrypt_bob(_bob: &mut BobModel) {
         // todo -- Logik für Verschlüsselung für Bob
     }
-    pub(crate) fn sign_bob(bob: &mut BobModel) {
+    pub(crate) fn sign_bob(_bob: &mut BobModel) {
         // todo -- Logik für Signierung für Bob
     }
-    pub(crate) fn decrypt_bob(bob: &mut BobModel) {
+    pub(crate) fn decrypt_bob(_bob: &mut BobModel) {
         // todo -- Logik für Entschlüsselung für Bob
     }
-    pub(crate) fn send_message_bob(bob: &mut BobModel) {
+    pub(crate) fn send_message_bob(_bob: &mut BobModel) {
         // todo -- Logik zum Senden der Nachricht für Bob
     }
-    pub(crate) fn clear_bob(bob: &mut BobModel) {
+    pub(crate) fn clear_bob(_bob: &mut BobModel) {
         // todo -- Logik zum Zurücksetzen der Eingabefelder und Labels für Bob
     }
 }
--- src/gui/view/view.rs
+++ src/gui/view/view.rs
@@ -1,6 +1,5 @@
 use crate::gui::controller::controller;
-use crate::gui::gui::CALCULATE_PUBLIC_KEY;
-use crate::gui::gui::{CLEAR, DECRYPT, ENCRYPT, SEND_MESSAGE, SIGN};
+
 use crate::gui::gui::{SWITCH_TO_ALICE, SWITCH_TO_BOB, SWITCH_TO_HAUPTMENU};
 use crate::gui::model::model::{AliceModel, BobModel, HauptMenuModel};
 use druid::{
@@ -24,7 +23,7 @@ pub(crate) fn build_haupt_menu() -> impl Widget<HauptMenuModel> {
 
     // Button
     let calc_public_key_button = Button::new("Berechne Öffentlichen Schlüssel").on_click(
-        |ctx, _data: &mut HauptMenuModel, _env| {
+        |_ctx, _data: &mut HauptMenuModel, _env| {
             controller::Controller::calculate_public_key(_data);
         },
     );
