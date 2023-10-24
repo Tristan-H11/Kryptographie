f83f1f101c93083c4af135805d52430114e67a5e
Simon Ganteföhr <simon.gantefoehr@edu.fhdw.de>
2023-10-24 17:56:28 +0200

    random.rs hinzugefügt (random als struct)

---
+++ src/encryption/math_functions/random.rs
@@ -0,0 +1,30 @@
+use bigdecimal::num_bigint::{BigInt, BigUint, ToBigInt};
+use bigdecimal::{BigDecimal, One, Zero};
+use rand::random;
+
+
+#[derive(Debug)]
+pub struct Random {
+    sqrt_m: BigDecimal,
+    n: BigDecimal,
+}
+
+impl Random {
+    pub fn create() -> Self {
+        let mut m = BigDecimal::from(random::<u128>());
+        while m.sqrt().unwrap() == BigDecimal::zero() {
+            m = BigDecimal::from(random::<u128>());
+        }
+        return Self {
+            sqrt_m: m.sqrt().unwrap(),
+            n: BigDecimal::zero(),
+        };
+    }
+
+    pub fn take(&mut self,a: &BigUint,b:&BigUint) -> BigUint {
+        self.n += BigDecimal::one();
+        let range = b-a+BigUint::one();
+        let num = (&self.n*&self.sqrt_m)%BigDecimal::one()*BigDecimal::from(BigInt::from(range));
+        return a + (BigDecimal::to_bigint(&num).unwrap()).to_biguint().unwrap();
+    }
+}
