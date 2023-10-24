8d9721e8a3e34ef55779d2ef46cb9303b90d0c2b
lucas <lucas.menke98@gmail.com>
2023-10-24 15:21:38 +0200

    block-chiffre -- change test_digits_from_vec_to_sum

--- src/tests/math_functions/test_block_chiffre.rs
+++ src/tests/math_functions/test_block_chiffre.rs
@@ -47,47 +47,37 @@ mod tests {
             vec![char_to_u16(' '), char_to_u16('T'), char_to_u16('e'), char_to_u16('s')],
             vec![char_to_u16('t'), char_to_u16('n'), char_to_u16('a'), char_to_u16('c')],
             vec![char_to_u16('h'), char_to_u16('r'), char_to_u16('i'), char_to_u16('c')],
-            vec![char_to_u16('h'), char_to_u16('t'), char_to_u16(' '), char_to_u16(' ')]
+            vec![char_to_u16('h'), char_to_u16('t'), char_to_u16(' '), char_to_u16(' ')],
         ];
         let result = string_to_int_vec(blocks);
         assert_eq!(result, expected);
     }
 
 
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use bigdecimal::num_bigint::BigUint;
-        use bigdecimal::ToPrimitive;
-        use crate::encryption::math_functions::block_chiffre::help_fun_sum_for_digits;
+    #[test]
+    fn test_digits_from_vec_to_sum() {
+        let digit_vectors = vec![
+            vec![char_to_u16('D'), char_to_u16('a'), char_to_u16('s'), char_to_u16(' ')],
+            vec![char_to_u16('i'), char_to_u16('s'), char_to_u16('t'), char_to_u16(' ')],
+            vec![char_to_u16('e'), char_to_u16('i'), char_to_u16('n'), char_to_u16('e')],
+            vec![char_to_u16(' '), char_to_u16('T'), char_to_u16('e'), char_to_u16('s')],
+            vec![char_to_u16('t'), char_to_u16('n'), char_to_u16('a'), char_to_u16('c')],
+            vec![char_to_u16('h'), char_to_u16('r'), char_to_u16('i'), char_to_u16('c')],
+            vec![char_to_u16('h'), char_to_u16('t'), char_to_u16(' '), char_to_u16(' ')],
+        ];
 
-        #[test]
-        fn test_digits_from_vec_to_sum() {
-            let digit_vectors = vec![
-                vec![char_to_u16('D'), char_to_u16('a'), char_to_u16('s'), char_to_u16(' ')],
-                vec![char_to_u16('i'), char_to_u16('s'), char_to_u16('t'), char_to_u16(' ')],
-                vec![char_to_u16('e'), char_to_u16('i'), char_to_u16('n'), char_to_u16('e')],
-                vec![char_to_u16(' '), char_to_u16('T'), char_to_u16('e'), char_to_u16('s')],
-                vec![char_to_u16('t'), char_to_u16('n'), char_to_u16('a'), char_to_u16('c')],
-                vec![char_to_u16('h'), char_to_u16('r'), char_to_u16('i'), char_to_u16('c')],
-                vec![char_to_u16('h'), char_to_u16('t'), char_to_u16(' '), char_to_u16(' ')]
-            ];
+        let result = digits_from_vec_to_sum(digit_vectors);
 
-            let result = digits_from_vec_to_sum(digit_vectors);
-
-            // Erwartete Summen für jeden Block berechnen
-            let expected_result = vec![
-                help_fun_sum_for_digits(&vec![char_to_u16('D'), char_to_u16('a'), char_to_u16('s'), char_to_u16(' ')]),
-                help_fun_sum_for_digits(&vec![char_to_u16('i'), char_to_u16('s'), char_to_u16('t'), char_to_u16(' ')]),
-                help_fun_sum_for_digits(&vec![char_to_u16('e'), char_to_u16('i'), char_to_u16('n'), char_to_u16('e')]),
-                help_fun_sum_for_digits(&vec![char_to_u16(' '), char_to_u16('T'), char_to_u16('e'), char_to_u16('s')]),
-                help_fun_sum_for_digits(&vec![char_to_u16('t'), char_to_u16('n'), char_to_u16('a'), char_to_u16('c')]),
-                help_fun_sum_for_digits(&vec![char_to_u16('h'), char_to_u16('r'), char_to_u16('i'), char_to_u16('c')]),
-                help_fun_sum_for_digits(&vec![char_to_u16('h'), char_to_u16('t'), char_to_u16(' '), char_to_u16(' ')])
-            ];
-
-            assert_eq!(result, expected_result);
-        }
+        let expected_result = vec![
+            BigUint::from(19140715035688992u64),
+            BigUint::from(29555366483460128u64),
+            BigUint::from(28429423626551397u64),
+            BigUint::from(9007560038613107u64),
+            BigUint::from(32651569751195747u64),
+            BigUint::from(29273887211061347u64),
+            BigUint::from(29273895796211744u64),
+        ];
+        assert_eq!(result, expected_result);
     }
 
 
@@ -110,3 +100,4 @@ mod tests {
         assert_eq!(result_sum, expected_sum);
     }
 }
+
