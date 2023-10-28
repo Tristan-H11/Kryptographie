0e71f8241b0f98deeea17971b9ce072fd3c02fa9
Tristan-H11 <triddel02@gmx.de>
2023-10-28 16:19:37 +0200

    Half und Double im Trait divisible ergänzt.

--- src/encryption/math_functions/traits/divisible.rs
+++ src/encryption/math_functions/traits/divisible.rs
@@ -1,5 +1,5 @@
 use bigdecimal::Zero;
-use std::ops::Rem;
+use std::ops::{Div, Mul, Rem};
 
 pub trait Divisible {
     ///
@@ -11,14 +11,54 @@ pub trait Divisible {
     /// Gibt zurück, ob die Zahl nicht durch eine andere teilbar ist.
     ///
     fn is_not_divisible_by(&self, divisor: &Self) -> bool;
+
+    ///
+    /// Gibt die Hälfte des Wertes zurück.
+    /// Achtung! 1.half() => 0
+    ///
+    fn half(&self) -> Self;
+
+    ///
+    /// Setzt den Wert auf die Hälfte des ursprünglichen Wertes.
+    /// Achtung! 1.half() => 0
+    ///
+    fn half_assign(&mut self);
+
+    ///
+    /// Gibt das Doppelte des Wertes zurück.
+    ///
+    fn double(&self) -> Self;
+
+    ///
+    /// Setzt den Wert auf das Doppelte des ursprünglichen Wertes.
+    ///
+    fn double_assign(&mut self);
 }
 
-impl<T: Zero + Rem<Output = T> + PartialEq + Clone> Divisible for T {
+impl<T: Zero + Rem<Output = T> + PartialEq + Clone + Div<Output = T> + Mul<Output = T> + From<u8>>
+    Divisible for T
+{
     fn is_divisible_by(&self, divisor: &Self) -> bool {
         self.clone() % divisor.clone() == T::zero()
     }
 
     fn is_not_divisible_by(&self, divisor: &Self) -> bool {
         !self.is_divisible_by(divisor)
+    }
+
+    fn half(&self) -> Self {
+        self.clone().div(T::from(2u8))
+    }
+
+    fn half_assign(&mut self) {
+        *self = self.half();
+    }
+
+    fn double(&self) -> Self {
+        self.clone() * T::from(2u8)
+    }
+
+    fn double_assign(&mut self) {
+        *self = self.double();
     }
 }
--- src/tests/math_functions/traits/test_divisible.rs
+++ src/tests/math_functions/traits/test_divisible.rs
@@ -34,4 +34,38 @@ mod tests {
             true
         );
     }
+
+    #[test]
+    fn test_half() {
+        assert_eq!(BigUint::from(8u32).half(), BigUint::from(4u32));
+        assert_eq!(BigUint::from(1u32).half(), BigUint::from(0u32));
+    }
+
+    #[test]
+    fn test_half_assign() {
+        let mut uint = BigUint::from(8u32);
+        uint.half_assign();
+        assert_eq!(uint, BigUint::from(4u32));
+
+        let mut uint = BigUint::from(1u32);
+        uint.half_assign();
+        assert_eq!(uint, BigUint::from(0u32));
+    }
+
+    #[test]
+    fn test_double() {
+        assert_eq!(BigUint::from(8u32).double(), BigUint::from(16u32));
+        assert_eq!(BigUint::from(1u32).double(), BigUint::from(2u32));
+    }
+
+    #[test]
+    fn test_double_assign() {
+        let mut uint = BigUint::from(8u32);
+        uint.double_assign();
+        assert_eq!(uint, BigUint::from(16u32));
+
+        let mut uint = BigUint::from(1u32);
+        uint.double_assign();
+        assert_eq!(uint, BigUint::from(2u32));
+    }
 }
