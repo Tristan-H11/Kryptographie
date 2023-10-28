b4b4c7038d23b9ce11d4361429767fe1ff889a1e
Tristan-H11 <triddel02@gmx.de>
2023-10-28 16:00:27 +0200

    Increment_Assign und Decrement_assign ergänzt.

--- src/encryption/math_functions/traits/increment.rs
+++ src/encryption/math_functions/traits/increment.rs
@@ -11,17 +11,35 @@ pub trait Increment {
     fn increment(&self) -> Self;
 
     ///
+    /// Erhöht den Wert um 1.
+    ///
+    fn increment_assign(&mut self);
+
+    ///
     /// Gibt das Dekrement des Wertes zurück.
     ///
     fn decrement(&self) -> Self;
+
+    ///
+    /// Verringert den Wert um 1.
+    ///
+    fn decrement_assign(&mut self);
 }
 
-impl<T: One + Clone + Add<Output = T> + Sub<Output = T>> Increment for T {
+impl<T: One + Clone + Add<Output=T> + Sub<Output=T>> Increment for T {
     fn increment(&self) -> Self {
         self.clone() + T::one()
     }
 
+    fn increment_assign(&mut self) {
+        *self = self.increment();
+    }
+
     fn decrement(&self) -> Self {
         self.clone() - T::one()
+    }
+
+    fn decrement_assign(&mut self) {
+        *self = self.decrement()
     }
 }
--- src/tests/math_functions/traits/test_increment.rs
+++ src/tests/math_functions/traits/test_increment.rs
@@ -14,4 +14,18 @@ mod tests {
         assert_eq!(BigUint::from(3u32).decrement(), BigUint::from(2u32));
         assert_eq!(BigUint::from(1u32).decrement(), BigUint::from(0u32));
     }
+
+    #[test]
+    fn test_increment_assign() {
+        let mut uint = BigUint::from(3u32);
+        uint.increment_assign();
+        assert_eq!(uint, BigUint::from(4u32));
+    }
+
+    #[test]
+    fn test_decrement_assign() {
+        let mut uint = BigUint::from(3u32);
+        uint.decrement_assign();
+        assert_eq!(uint, BigUint::from(2u32));
+    }
 }
