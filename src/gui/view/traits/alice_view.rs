619ffd990d62955123702d72bb17c1485d38ca62
lucas <lucas.menke98@gmail.com>
2023-10-31 09:50:34 +0100

    Gui -- optische Anpassungen

--- src/gui/view/traits/alice_view.rs
+++ src/gui/view/traits/alice_view.rs
@@ -8,30 +8,30 @@ use crate::gui::controller::commands::{
     CLEAR, DECRYPT, ENCRYPT, SEND_MESSAGE, SIGN, SWITCH_TO_MAIN_MENU, VERIFY,
 };
 use crate::gui::model::model::AliceModel;
-use crate::gui::view::traits::common_view_builder::{CommonViewComponents, ViewBuilder};
+use crate::gui::view::traits::common_view_builder::{CommonViewComponentsDefault, ViewBuilder};
 
 pub struct AliceViewBuilder;
 
 impl ViewBuilder<AliceModel> for AliceViewBuilder {
     fn build_view() -> SizedBox<AliceModel> {
         //verwende gemeinsame Komponenten s.o.
-        let common_components = CommonViewComponents::new();
+        let common_components = CommonViewComponentsDefault::new();
 
-        let plaintext_entry = common_components.create_text_entry(
+        let plaintext_entry = common_components.create_text_entry_default(
             "Klartext: ",
             "z.B. Hallo wie geht es dir?",
             false,
             AliceModel::plaintext,
         );
 
-        let ciphertext_entry = common_components.create_text_entry(
+        let ciphertext_entry = common_components.create_text_entry_default(
             "Geheimtext: ",
             "Wird automatisch berechnet",
             false,
             AliceModel::ciphertext,
         );
 
-        let secret_exponent_entry = common_components.create_text_entry(
+        let secret_exponent_entry = common_components.create_text_entry_default(
             "Geheimer Exponent: ",
             "Wird automatisch berechnet",
             true,
@@ -39,22 +39,22 @@ impl ViewBuilder<AliceModel> for AliceViewBuilder {
         );
 
         let signature_row =
-            common_components.create_text_entry("Signatur: ", "", true,AliceModel::signature);
+            common_components.create_text_entry_default("Signatur: ", "", true, AliceModel::signature);
 
         let encrypt_button =
-            common_components.create_button("Mit Bobs PublicKey verschlüsseln", ENCRYPT);
-        let calc_sign_button = common_components.create_button("Signatur berechnen", SIGN);
-        let check_sign_button = common_components.create_button("Signatur prüfen", VERIFY);
+            common_components.create_button_default("Mit Bobs PublicKey verschlüsseln", ENCRYPT);
+        let calc_sign_button = common_components.create_button_default("Signatur berechnen", SIGN);
+        let check_sign_button = common_components.create_button_default("Signatur prüfen", VERIFY);
         let decrypt_button =
-            common_components.create_button("Mit eigenem PrivateKey entschlüsseln", DECRYPT);
-        let send_message_button = common_components.create_button("Nachricht senden", SEND_MESSAGE);
+            common_components.create_button_default("Mit eigenem PrivateKey entschlüsseln", DECRYPT);
+        let send_message_button = common_components.create_button_default("Nachricht senden", SEND_MESSAGE);
         let clear_button =
-            common_components.create_button("Alles außer privaten Schlüssel löschen", CLEAR);
+            common_components.create_button_default("Alles außer privaten Schlüssel löschen", CLEAR);
         let back_button =
-            common_components.create_button("Zurück zum Hauptmenü", SWITCH_TO_MAIN_MENU);
+            common_components.create_button_default("Zurück zum Hauptmenü", SWITCH_TO_MAIN_MENU);
 
         Flex::column()
-            .with_flex_spacer(common_components.flex_space)
+            .with_flex_spacer(common_components.flex_space_default)
             .with_flex_child(plaintext_entry, 0.2)
             .with_flex_child(ciphertext_entry, 0.2)
             .with_flex_child(encrypt_button, 0.1)
--- src/gui/view/traits/main_menu_view.rs
+++ src/gui/view/traits/main_menu_view.rs
@@ -5,31 +5,31 @@ use crate::gui::controller::commands::{
     CALCULATE_KEYPAIR_ALICE, CALCULATE_KEYPAIR_BOB, SWITCH_TO_ALICE, SWITCH_TO_BOB,
 };
 use crate::gui::model::model::MainMenuModel;
-use crate::gui::view::traits::common_view_builder::{CommonViewComponents, ViewBuilder};
+use crate::gui::view::traits::common_view_builder::{CommonViewComponentsDefault, ViewBuilder};
 
 // trait für Hauptmenü
 pub struct MainMenuViewBuilder;
 
 impl ViewBuilder<MainMenuModel> for MainMenuViewBuilder {
     fn build_view() -> SizedBox<MainMenuModel> {
-        let common_components = CommonViewComponents::new(); // Gemeinsame Komponenten instanziieren
+        let common_components = CommonViewComponentsDefault::new(); // Gemeinsame Komponenten instanziieren
 
         // Entry-Felder
-        let modul_width_entry = common_components.create_text_entry(
+        let modul_width_entry = common_components.create_text_entry_default(
             "Breite des Moduls n: ",
             "z.B. 4096",
             false,
             MainMenuModel::modul_width,
         );
 
-        let miller_rabin_entry = common_components.create_text_entry(
+        let miller_rabin_entry = common_components.create_text_entry_default(
             "Miller-Rabin Iterationen: ",
             "z.B. 100",
             false,
             MainMenuModel::miller_rabin_iterations,
         );
 
-        let basis_entry = common_components.create_text_entry(
+        let basis_entry = common_components.create_text_entry_default(
             "Basis für Kalkulationen: ",
             "Der Wert des höchsten Unicode-Zeichens exklusive, z.B. 55926",
             false,
@@ -37,43 +37,43 @@ impl ViewBuilder<MainMenuModel> for MainMenuViewBuilder {
         );
 
         // Button
-        let calculate_keypair_alice = common_components.create_button(
+        let calculate_keypair_alice = common_components.create_button_default(
             "Schlüsselpaar <public, private> Alice berechnen",
             CALCULATE_KEYPAIR_ALICE,
         );
 
-        let calculate_keypair_bob = common_components.create_button(
+        let calculate_keypair_bob = common_components.create_button_default(
             "Schlüsselpaar <public, private> Bob berechnen",
             CALCULATE_KEYPAIR_BOB,
         );
 
         let open_alice_button =
-            common_components.create_button("Alice-Ansicht öffnen", SWITCH_TO_ALICE);
+            common_components.create_button_default("Alice-Ansicht öffnen", SWITCH_TO_ALICE);
 
-        let open_bob_button = common_components.create_button("Bob-Ansicht öffnen", SWITCH_TO_BOB);
+        let open_bob_button = common_components.create_button_default("Bob-Ansicht öffnen", SWITCH_TO_BOB);
 
-        let public_exponent_bob_entry = common_components.create_text_entry(
+        let public_exponent_bob_entry = common_components.create_text_entry_default(
             "Öffentlicher Exponent Bob: ",
             "Wird automatisch berechnet",
             true,
             MainMenuModel::public_exponent_bob,
         );
 
-        let public_exponent_alice_entry = common_components.create_text_entry(
+        let public_exponent_alice_entry = common_components.create_text_entry_default(
             "Öffentlicher Exponent Alice: ",
             "Wird automatisch berechnet",
             true,
             MainMenuModel::public_exponent_alice,
         );
 
-        let modul_alice_entry = common_components.create_text_entry(
+        let modul_alice_entry = common_components.create_text_entry_default(
             "Modul Alice: ",
             "Wird automatisch berechnet",
             true,
             MainMenuModel::modul_alice,
         );
 
-        let modul_bob_entry = common_components.create_text_entry(
+        let modul_bob_entry = common_components.create_text_entry_default(
             "Modul Bob: ",
             "Wird automatisch berechnet",
             true,
@@ -83,7 +83,7 @@ impl ViewBuilder<MainMenuModel> for MainMenuViewBuilder {
 
         // UI Struktur
         Flex::column()
-            .with_flex_spacer(common_components.flex_space)
+            .with_flex_spacer(common_components.flex_space_default)
             .with_flex_child(modul_width_entry, 0.2)
             .with_flex_child(miller_rabin_entry, 0.2)
             .with_flex_child(basis_entry, 0.2)
