e758c1ec1ecdfb4ee9e5082faec91e1fabda4a37
lucas <lucas.menke98@gmail.com>
2023-10-31 09:28:20 +0100

    Gui -- main.rs aufräumen und gui code an richtige stelle gepackt

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use gui::gui::build_ui;
 use gui::model::model::AppState;
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
+use crate::gui::gui::start_gui;
 
 fn main() {
     SimpleLogger::new()
@@ -15,29 +16,5 @@ fn main() {
         .init()
         .unwrap();
 
-    let main_window = WindowDesc::new(build_ui())
-        .title("RSA-Simulator")
-        .resizable(true);
-    let initial_state = AppState::default();
-    AppLauncher::with_window(main_window)
-        .configure_env(|env, _| {
-            let background_color = Color::from_hex_str("#2a2a2a").unwrap();
-            let button_color = Color::from_hex_str("#607d8b").unwrap();
-            let button_color_pressed = Color::from_hex_str("#2b3f49").unwrap();
-            let text_color = Color::from_hex_str("#FFFFFF").unwrap();
-
-            env.set(theme::TEXTBOX_BORDER_RADIUS, 0.0);
-            env.set(theme::TEXTBOX_BORDER_WIDTH, 0.0);
-
-            env.set(theme::WINDOW_BACKGROUND_COLOR, background_color);
-            env.set(theme::TEXT_COLOR, text_color);
-            env.set(theme::BUTTON_LIGHT, button_color);
-            env.set(theme::BUTTON_DARK, button_color_pressed);
-            env.set(theme::BUTTON_BORDER_RADIUS, 0.0);
-            env.set(theme::BUTTON_BORDER_WIDTH, 0.0);
-
-            env.set(theme::UI_FONT, FontDescriptor::default().with_size(14.0));
-        })
-        .launch(initial_state)
-        .expect("Failed to launch application");
+    start_gui();
 }
