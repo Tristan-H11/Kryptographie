4dc24cb8b3d7a05ba65dca51f9e140c9b2862c0b
Tristan-H11 <triddel02@gmx.de>
2023-11-14 14:18:16 +0100

    Keys um Konstruktor mit Blockgröße statt basis erweitert.

--- src/encryption/rsa/keys.rs
+++ src/encryption/rsa/keys.rs
@@ -27,10 +27,26 @@ impl PublicKey {
     ///
     /// * `e` - Der öffentliche Exponent.
     /// * `n` - Das Produkt der beiden Primzahlen.
+    /// * `g_base` - Die Basis, in der die Nachricht verschlüsselt werden soll.
     ///
     pub fn new(e: BigInt, n: BigInt, g_base: u32) -> PublicKey {
         // Maximale Blockbreite = log_g(n).
         let block_size = n.log(&big_i!(g_base));
+        debug!("Blocksize in der PublicKey-Erstellung: {}", block_size);
+        PublicKey { e, n, block_size }
+    }
+
+    ///
+    /// Erstellt eine neue Instanz des PublicKey.
+    ///
+    /// # Argumente
+    ///
+    /// * `e` - Der öffentliche Exponent.
+    /// * `n` - Das Produkt der beiden Primzahlen.
+    /// * `block_size` - Die Blockgröße.
+    ///
+    pub fn new_with_blocksize(e: BigInt, n: BigInt, block_size: usize) -> PublicKey {
+        // Maximale Blockbreite = log_g(n).
         debug!("Blocksize in der PublicKey-Erstellung: {}", block_size);
         PublicKey { e, n, block_size }
     }
@@ -128,10 +144,26 @@ impl PrivateKey {
     ///
     /// * `d` - Der private Exponent.
     /// * `n` - Das Produkt der beiden Primzahlen.
+    /// * `g_base` - Die Basis, in der die Nachricht verschlüsselt werden soll.
     ///
     pub fn new(d: BigInt, n: BigInt, g_base: u32) -> PrivateKey {
         // Die Größe der verschlüsselten Blöcke ist immer um 1 größer als die Klartextgröße.
         let block_size = n.log(&big_i!(g_base)) + 1;
+        debug!("Blocksize in der PrivateKey-Erstellung: {}", block_size);
+        PrivateKey { d, n, block_size }
+    }
+
+    ///
+    /// Erstellt eine neue Instanz des PrivateKey.
+    ///
+    /// # Argumente
+    ///
+    /// * `d` - Der private Exponent.
+    /// * `n` - Das Produkt der beiden Primzahlen.
+    /// * `block_size` - Die Blockgröße.
+    ///
+    pub fn new_with_blocksize(d: BigInt, n: BigInt, block_size: usize) -> PrivateKey {
+        // Die Größe der verschlüsselten Blöcke ist immer um 1 größer als die Klartextgröße.
         debug!("Blocksize in der PrivateKey-Erstellung: {}", block_size);
         PrivateKey { d, n, block_size }
     }
