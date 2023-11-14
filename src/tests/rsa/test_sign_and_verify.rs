261688ba3faeae35bb9acda0f2318a3776b22832
Tristan-H11 <triddel02@gmx.de>
2023-11-14 16:54:31 +0100

    Verify-Tests erweitert.

--- src/tests/rsa/test_sign_and_verify.rs
+++ src/tests/rsa/test_sign_and_verify.rs
@@ -1,24 +1,49 @@
 #[cfg(test)]
 mod sign_verify_test {
+    use std::str::FromStr;
     use crate::encryption::rsa::keys::{PrivateKey, PublicKey};
     use bigdecimal::num_bigint::BigInt;
+    use crate::encryption::math_functions::number_theory::fast_exponentiation;
+    use crate::encryption::rsa::rsa_keygen_service::RsaKeygenService;
 
     #[test]
-    fn test_sign_and_verify_flow() {
-        let e = BigInt::from(65537);
-        let n = BigInt::from(43);
-        let d = BigInt::from(47);
+    fn test_sign_and_verify_lowest_possible_happyflow() {
+        let keygen = RsaKeygenService::new(257);
+
         let g_base = 55296;
 
-        let public_key = PublicKey::new(e.clone(), n.clone(), g_base);
-        let private_key = PrivateKey::new(d.clone(), n.clone(), g_base);
+        let (public_key, private_key) = keygen.generate_keypair(
+            10,
+            13,
+            g_base
+        );
 
-        let message = "Hallo meine 123 ! kleine.";
+        let message = "Die Nachricht soll signiert werden.";
 
         let signature = private_key.sign(&message);
-        println!("Signatur: {}", signature);
 
         let is_valid = public_key.verify(&signature, &message);
         assert!(is_valid);
+    }
+
+    #[test]
+    fn test_sign_and_verify_highest_unhappy_flow() {
+        let keygen = RsaKeygenService::new(256);
+
+        let g_base = 55296;
+
+        let (public_key, private_key) = keygen.generate_keypair(
+            10,
+            13,
+            g_base
+        );
+
+        let message = "Die Nachricht soll signiert werden.";
+
+        let signature = private_key.sign(&message);
+
+        let is_valid = public_key.verify(&signature, &message);
+        // Assert NOT is_valid, because the key is too small and thus the message is truncated.
+        assert!(!is_valid);
     }
 }
