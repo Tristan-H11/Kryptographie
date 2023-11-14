5bef7b2ac48dbea44521e6ffc834827df59033fc
Tristan-H11 <triddel02@gmx.de>
2023-11-14 19:59:38 +0100

    Zwischenstand sidenav

--- GUI/src/app/app.component.ts
+++ GUI/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {SidemenuComponent} from "./sidemenu/sidemenu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SidemenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
