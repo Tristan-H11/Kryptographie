75a638ae0dc66c0211dcea9efcf63b55e43593f8
Tristan-H11 <triddel02@gmx.de>
2023-11-14 20:21:44 +0100

    Sidenav eine Komponente höher gezogen und Simons Namen korrigiert.

--- GUI/src/app/app.component.ts
+++ GUI/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import {SidemenuComponent} from "./sidemenu/sidemenu.component";
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatButtonModule} from "@angular/material/button";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SidemenuComponent],
+  imports: [CommonModule, RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule, MatSidenavModule, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
