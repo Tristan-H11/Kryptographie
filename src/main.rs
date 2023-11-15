f401ca0eef291a0cfe1543fd6e98683dd20d40d3
Tristan-H11 <triddel02@gmx.de>
2023-11-15 15:36:57 +0100

    Alle Zugriffsursprünge auf die http API erlaubt.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod encryption;
 mod tests;
 mod rest;
 
+use actix_cors::Cors;
 use actix_web::{App, HttpServer};
 use actix_web::middleware::Logger;
 use log::LevelFilter;
@@ -22,6 +23,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .configure(config_app)
             .wrap(Logger::default())
+            .wrap(Cors::permissive())
     })
         .bind(("127.0.0.1", 8080))?
         .run()
