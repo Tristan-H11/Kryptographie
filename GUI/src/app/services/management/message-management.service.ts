044a08b861db4ed9ed4d62ce77d3a5121fdff253
Tristan-H11 <triddel02@gmx.de>
2023-11-15 16:52:52 +0100

    Manager verschoben

---
+++ GUI/src/app/services/management/message-management.service.ts
@@ -0,0 +1,41 @@
+import {Injectable} from '@angular/core';
+import {createEmptyMessageSignatureContainer, MessageSignatureContainer} from "../../models/message-signature-container";
+import {ClientEnum} from "../../models/client-enum";
+import {AbstractClientObservableManagementService} from "./abstract-client-observable-management-service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageManagementService extends AbstractClientObservableManagementService<MessageSignatureContainer>{
+  protected override createEmptyObject(): MessageSignatureContainer {
+      return createEmptyMessageSignatureContainer();
+  }
+
+  constructor() {
+      super();
+  }
+
+  public setPlaintext(plaintext: string, client: ClientEnum): void {
+    this.setProperty(client, "plaintext", plaintext);
+  }
+
+  public getPlaintext(client: ClientEnum): string {
+    return this.getPropertyAsString(client, "plaintext");
+  }
+
+  public setCiphertext(ciphertext: string, client: ClientEnum): void {
+    this.setProperty(client, "ciphertext", ciphertext);
+  }
+
+  public getCiphertext(client: ClientEnum): string {
+    return this.getPropertyAsString(client, "ciphertext");
+  }
+
+  public setSignature(signature: string, client: ClientEnum): void {
+    this.setProperty(client, "signature", signature);
+  }
+
+  public getSignature(client: ClientEnum): string {
+    return this.getPropertyAsString(client, "signature");
+  }
+}
