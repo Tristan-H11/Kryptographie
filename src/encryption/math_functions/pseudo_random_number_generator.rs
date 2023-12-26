e1a3aa6c459fa817e047de78b56035ae21b0b04f
Tristan-H11 <triddel02@gmx.de>
2023-12-26 21:58:53 +0100

    Miller_Rabin mit atomicCounter umgebaut. Sollte nun alles fix und sicher klappen.

--- src/encryption/math_functions/pseudo_random_number_generator.rs
+++ src/encryption/math_functions/pseudo_random_number_generator.rs
@@ -1,3 +1,4 @@
+use atomic_counter::{AtomicCounter, RelaxedCounter};
 use bigdecimal::num_bigint::{BigInt, ToBigInt};
 use bigdecimal::{BigDecimal, One};
 use log::trace;
@@ -46,22 +47,23 @@ impl PseudoRandomNumberGenerator {
     }
 
     /// Diese Methode gibt eine Zufallszahl im Bereich von a bis b zurück.
+    /// Inkrementiert den AtomicCounter n_counter!
     ///
     /// # Argumente
     /// * `a` - Die untere Grenze des Bereichs.
     /// * `b` - Die obere Grenze des Bereichs.
-    /// * `n` - Index des Elementes aus der Zufallsfolge.
+    /// * `n_counter` - Der AtomicCounter, der den Index des Elementes aus der Zufallsfolge enthält.
     ///
     /// # Rückgabe
     /// * BigUint
-    pub fn take(&self, a: &BigInt, b: &BigInt, n: usize) -> BigInt {
+    pub fn take(&self, a: &BigInt, b: &BigInt, n_counter: &RelaxedCounter) -> BigInt {
         trace!(
             "Zufallszahl aus dem Bereich von {} bis {} mit n {}",
             a,
             b,
-            n
+            n_counter.get()
         );
-        let factor = (big_d!(n as u64) * &self.sqrt_m) % BigDecimal::one();
+        let factor = (big_d!(n_counter.inc() as u64) * &self.sqrt_m) % BigDecimal::one();
         let range = big_d!(b - a + BigInt::one());
 
         // Das unwrap() wird niemals fehlschlagen, weil die Implementation von to_bigint() nur
@@ -70,27 +72,28 @@ impl PseudoRandomNumberGenerator {
     }
 
     /// Diese Methode gibt eine ungerade Zufallszahl im Bereich von a bis b zurück.
+    /// Inkrementiert den AtomicCounter n_counter!
     ///
     /// # Argumente
     /// * `a` - Die untere Grenze des Bereichs.
     /// * `b` - Die obere Grenze des Bereichs.
-    /// * `n` - Index des Elementes aus der Zufallsfolge.
+    /// * `n_counter` - Index des Elementes aus der Zufallsfolge.
     ///
     /// # Rückgabe
     /// * BigUint
-    pub fn take_uneven(&self, a: &BigInt, b: &BigInt, n: usize) -> BigInt {
-        self.take(a, b, n) | BigInt::one()
+    pub fn take_uneven(&self, a: &BigInt, b: &BigInt, n_counter: &RelaxedCounter) -> BigInt {
+        self.take(a, b, n_counter) | BigInt::one()
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use atomic_counter::RelaxedCounter;
     use bigdecimal::num_bigint::BigInt;
 
     use crate::big_i;
     use crate::encryption::math_functions::pseudo_random_number_generator::PseudoRandomNumberGenerator;
     use crate::encryption::math_functions::traits::divisible::Divisible;
-    use crate::encryption::math_functions::traits::increment::Increment;
 
     #[test]
     fn test_happy_flow() {
@@ -99,27 +102,18 @@ mod tests {
 
         let random = PseudoRandomNumberGenerator::new(&big_i!(13));
 
-        let mut n = 1;
+        let n = RelaxedCounter::new(1);
 
-        assert_eq!(random.take(&a, &b, n), 604u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 211u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 815u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 421u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 28u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 632u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 239u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 842u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 449u32.into());
-        n.increment_assign();
-        assert_eq!(random.take(&a, &b, n), 56u32.into());
+        assert_eq!(random.take(&a, &b, &n), 604u32.into());
+        assert_eq!(random.take(&a, &b, &n), 211u32.into());
+        assert_eq!(random.take(&a, &b, &n), 815u32.into());
+        assert_eq!(random.take(&a, &b, &n), 421u32.into());
+        assert_eq!(random.take(&a, &b, &n), 28u32.into());
+        assert_eq!(random.take(&a, &b, &n), 632u32.into());
+        assert_eq!(random.take(&a, &b, &n), 239u32.into());
+        assert_eq!(random.take(&a, &b, &n), 842u32.into());
+        assert_eq!(random.take(&a, &b, &n), 449u32.into());
+        assert_eq!(random.take(&a, &b, &n), 56u32.into());
 
         let a: BigInt = 500u32.into();
         let b: BigInt = 6000u32.into();
@@ -127,8 +121,7 @@ mod tests {
         let random = PseudoRandomNumberGenerator::new(&big_i!(40));
 
         for _ in 1..500 {
-            let random = random.take(&a, &b, n);
-            n.increment_assign();
+            let random = random.take(&a, &b, &n);
             assert!(random >= a && random <= b);
         }
     }
@@ -140,11 +133,10 @@ mod tests {
 
         let random = PseudoRandomNumberGenerator::new(&big_i!(23));
 
-        let mut n = 1;
+        let n = RelaxedCounter::new(1);
 
         for _ in 1..500 {
-            let random = random.take_uneven(&a, &b, n);
-            n.increment_assign();
+            let random = random.take_uneven(&a, &b, &n);
             assert!(random >= a && random <= b);
             assert!(random.is_not_divisible_by(&BigInt::from(2)));
         }
