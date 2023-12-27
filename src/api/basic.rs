519ca4ba7e94060481b86336dffb9f89f69151f0
Tristan-H11 <triddel02@gmx.de>
2023-12-27 19:52:14 +0100

    API Package neu geordnet.

--- src/api/basic.rs
+++ src/api/basic.rs
@@ -1,15 +1,16 @@
-use crate::api::create_key_pair::create_key_pair;
-use crate::api::decrypt::decrypt;
-use crate::api::encrypt::encrypt;
-use crate::api::exponentiation::exponentiation;
-use crate::api::extended_euclid::euclid_endpoint;
-use crate::api::modular_inverse::modular_inverse_endpoint;
-use crate::api::shanks::shanks_endpoint;
-use crate::api::sign::sign;
-use crate::api::verify::verify;
-use actix_web::{web, HttpResponse, Responder};
+use actix_web::{HttpResponse, Responder, web};
 use log::info;
 use serde::Serialize;
+
+use crate::api::endpoints::create_key_pair::create_key_pair;
+use crate::api::endpoints::decrypt::decrypt;
+use crate::api::endpoints::encrypt::encrypt;
+use crate::api::endpoints::exponentiation::exponentiation;
+use crate::api::endpoints::extended_euclid::euclid_endpoint;
+use crate::api::endpoints::modular_inverse::modular_inverse_endpoint;
+use crate::api::endpoints::shanks::shanks_endpoint;
+use crate::api::endpoints::sign::sign;
+use crate::api::endpoints::verify::verify;
 
 #[derive(Serialize)]
 pub struct Response {
