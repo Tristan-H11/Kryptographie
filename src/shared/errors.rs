67392d3a1161755a34e565171c0fb064242639e4
Tristan-H11 <triddel02@gmx.de>
2024-01-03 15:53:20 +0100

    NoDiscreteLogarithmError hinzugefügt.

--- src/shared/errors.rs
+++ src/shared/errors.rs
@@ -8,12 +8,20 @@ pub enum ArithmeticError {
     /// * `number` - Die Zahl, die kein Inverses hat.
     /// * `modulus` - Das Modulus, zu dem die Zahl kein Inverses hat.
     NoInverseError(String, String),
+
+    /// Wird geworfen, wenn eine Zahl keinen diskreten Logarithmus hat.
+    ///
+    /// # Argumente
+    /// * `base` - Die Basis, zu der der diskrete Logarithmus nicht existiert.
+    /// * `element` - Das Element, zu dem der diskrete Logarithmus nicht existiert.
+    NoDiscreteLogarithmError(String, String),
 }
 
 impl fmt::Display for ArithmeticError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             ArithmeticError::NoInverseError(number, modulus) => write!(f, "No inverse error: number {} with modulus {}", number, modulus),
+            ArithmeticError::NoDiscreteLogarithmError(base, element) => write!(f, "No discrete logarithm error: base {} with element {}", base, element),
         }
     }
 }
