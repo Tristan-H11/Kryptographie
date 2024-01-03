709670bb57df41b194969dc6a2dc757545058f1e
Tristan-H11 <triddel02@gmx.de>
2024-01-03 16:38:18 +0100

    ExtendedEuclid auf den checked call umgebaut

--- src/api/endpoints/math/extended_euclid.rs
+++ src/api/endpoints/math/extended_euclid.rs
@@ -1,12 +1,13 @@
 use std::str::FromStr;
 
-use actix_web::web::{Json, Query};
 use actix_web::{HttpResponse, Responder};
+use actix_web::web::{Json, Query};
 use bigdecimal::num_bigint::BigInt;
 use log::info;
 use serde::{Deserialize, Serialize};
 
-use crate::api::serializable_models::{SingleStringResponse, UseFastQuery};
+use crate::api::basic::call_checked_with_parsed_big_ints;
+use crate::api::serializable_models::UseFastQuery;
 use crate::encryption::math_functions::number_theory::extended_euclid_result::ExtendedEuclidResult;
 use crate::encryption::math_functions::number_theory::number_theory_service::{NumberTheoryService, NumberTheoryServiceTrait};
 use crate::encryption::math_functions::number_theory::number_theory_service::NumberTheoryServiceSpeed::{Fast, Slow};
@@ -54,25 +55,18 @@ pub(crate) async fn euclid_endpoint(
     let req_body: ExtendedEuclidRequest = req_body.into_inner();
     let use_fast = query.use_fast;
 
-    let a = &BigInt::from_str(&*req_body.a);
-    let b = &BigInt::from_str(&*req_body.b);
+    call_checked_with_parsed_big_ints(|| {
+        let a = &BigInt::from_str(&*req_body.a)?;
+        let b = &BigInt::from_str(&*req_body.b)?;
 
-    let number_theory_service = match use_fast {
-        true => NumberTheoryService::new(Fast),
-        false => NumberTheoryService::new(Slow),
-    };
+        let number_theory_service = match use_fast {
+            true => NumberTheoryService::new(Fast),
+            false => NumberTheoryService::new(Slow),
+        };
 
-    match (a, b) {
-        (Ok(a), Ok(b)) => {
-            let extended_euclid_result = number_theory_service.extended_euclid(a, b);
+        let extended_euclid_result = number_theory_service.extended_euclid(a, b);
 
-            let response = ExtendedEuclidResponse::from(extended_euclid_result);
-            HttpResponse::Ok().json(response)
-        },
-        _ => return HttpResponse::BadRequest().json(SingleStringResponse {
-            message: "Fehler beim Parsen der Parameter".to_string(),
-        }),
-    }
-
-
+        let response = ExtendedEuclidResponse::from(extended_euclid_result);
+        Ok(HttpResponse::Ok().json(response))
+    })
 }
