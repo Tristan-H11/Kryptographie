8a1f26848717d271bc06f6ddd9926cca59f71581
Tristan-H11 <triddel02@gmx.de>
2024-01-03 16:44:49 +0100

    Decrypt Endpunkt auf den checked call umgebaut

--- src/api/endpoints/rsa/decrypt.rs
+++ src/api/endpoints/rsa/decrypt.rs
@@ -1,6 +1,7 @@
 use actix_web::web::{Json, Query};
 use actix_web::{HttpResponse, Responder};
 use log::info;
+use crate::api::basic::call_checked_with_parsed_big_ints;
 
 use crate::api::serializable_models::{EncryptDecryptRequest, SingleStringResponse, UseFastQuery};
 use crate::encryption::math_functions::number_theory::number_theory_service::NumberTheoryService;
@@ -23,20 +24,22 @@ pub(crate) async fn decrypt(
     );
     let req_body: EncryptDecryptRequest = req_body.into_inner();
     let use_fast = query.use_fast;
-
     let ciphertext = req_body.message;
-    let private_key = req_body.key_pair.to_private_key();
     let number_system_base = req_body.number_system_base;
 
-    let number_theory_service = match use_fast {
-        true => NumberTheoryService::new(Fast),
-        false => NumberTheoryService::new(Slow),
-    };
+    call_checked_with_parsed_big_ints(|| {
+        let private_key = req_body.key_pair.to_private_key()?;
 
-    let rsa_service = crate::encryption::rsa::rsa_service::RsaService::new(number_theory_service);
+        let number_theory_service = match use_fast {
+            true => NumberTheoryService::new(Fast),
+            false => NumberTheoryService::new(Slow),
+        };
 
-    let plaintext = rsa_service.decrypt(&ciphertext, number_system_base, &private_key);
-    let response = SingleStringResponse { message: plaintext };
+        let rsa_service = crate::encryption::rsa::rsa_service::RsaService::new(number_theory_service);
 
-    HttpResponse::Ok().json(response)
+        let plaintext = rsa_service.decrypt(&ciphertext, number_system_base, &private_key);
+        let response = SingleStringResponse { message: plaintext };
+
+        Ok(HttpResponse::Ok().json(response))
+    })
 }
