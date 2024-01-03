0a642c53729de89dc64092eaf5293898b9f577c9
Tristan-H11 <triddel02@gmx.de>
2024-01-03 15:47:52 +0100

    RsaError::KeyGenerationError erstellt.

--- src/shared/errors.rs
+++ src/shared/errors.rs
@@ -17,3 +17,19 @@ impl fmt::Display for ArithmeticError {
         }
     }
 }
+
+impl std::error::Error for ArithmeticError {}
+
+#[derive(Debug, Clone)]
+pub enum RsaError {
+    /// Wird geworfen, wenn die Schlüsselerzeugung fehlschlägt.
+    KeyGenerationError
+}
+
+impl fmt::Display for RsaError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            RsaError::KeyGenerationError => write!(f, "RSA Key generation error"),
+        }
+    }
+}
