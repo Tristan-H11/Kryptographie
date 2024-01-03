d1b6acff55221e3345508f14bf5e932a755269b7
Tristan-H11 <triddel02@gmx.de>
2024-01-03 16:00:59 +0100

    Extended Euclid Endpunkt auf robustes Parsen der Parameter umgestellt.

--- src/api/endpoints/math/extended_euclid.rs
+++ src/api/endpoints/math/extended_euclid.rs
@@ -6,7 +6,7 @@ use bigdecimal::num_bigint::BigInt;
 use log::info;
 use serde::{Deserialize, Serialize};
 
-use crate::api::serializable_models::{UseFastQuery};
+use crate::api::serializable_models::{SingleStringResponse, UseFastQuery};
 use crate::encryption::math_functions::number_theory::extended_euclid_result::ExtendedEuclidResult;
 use crate::encryption::math_functions::number_theory::number_theory_service::{NumberTheoryService, NumberTheoryServiceTrait};
 use crate::encryption::math_functions::number_theory::number_theory_service::NumberTheoryServiceSpeed::{Fast, Slow};
@@ -54,17 +54,25 @@ pub(crate) async fn euclid_endpoint(
     let req_body: ExtendedEuclidRequest = req_body.into_inner();
     let use_fast = query.use_fast;
 
-    let a = &BigInt::from_str(&*req_body.a).unwrap();
-    let b = &BigInt::from_str(&*req_body.b).unwrap();
+    let a = &BigInt::from_str(&*req_body.a);
+    let b = &BigInt::from_str(&*req_body.b);
 
     let number_theory_service = match use_fast {
         true => NumberTheoryService::new(Fast),
         false => NumberTheoryService::new(Slow),
     };
 
-    let extended_euclid_result = number_theory_service.extended_euclid(a, b);
+    match (a, b) {
+        (Ok(a), Ok(b)) => {
+            let extended_euclid_result = number_theory_service.extended_euclid(a, b);
 
-    let response = ExtendedEuclidResponse::from(extended_euclid_result);
+            let response = ExtendedEuclidResponse::from(extended_euclid_result);
+            HttpResponse::Ok().json(response)
+        },
+        _ => return HttpResponse::BadRequest().json(SingleStringResponse {
+            message: "Fehler beim Parsen der Parameter".to_string(),
+        }),
+    }
 
-    HttpResponse::Ok().json(response)
+
 }
