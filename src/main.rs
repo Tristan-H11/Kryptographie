f2cdfd5f1f78e74813e463326faedab560a07490
tristanhormann <triddel02@gmx.de>
2024-01-11 16:12:23 +0100

    Server bindet sich nun an 0.0.0.0

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .wrap(Cors::permissive())
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
