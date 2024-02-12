76de52e2d81610fbbdc146aa536d57ba08372f2c
Tristan-H11 <triddel02@gmx.de>
2024-02-12 12:47:34 +0100

    Elliptische Kurven: Verdopplung von Punkten implementiert und im Add den Fall abgefangen, dass identische Punkte addiert werden.

--- src/encryption/ecc/ec_point.rs
+++ src/encryption/ecc/ec_point.rs
@@ -16,7 +16,7 @@ pub struct EcPoint {
     // Die Koordinaten des Punktes
     pub x: BigInt,
     pub y: BigInt,
-    // Statische Referenz auf die elliptische Kurve, auf der der Punkt liegt
+    // Referenz auf die elliptische Kurve, auf der der Punkt liegt
     pub curve: Rc<EllipticCurve>,
 }
 
@@ -28,47 +28,63 @@ impl EcPoint {
     ///
     /// Addiert zwei Punkte auf einer elliptischen Kurve.
     /// Die Punkte müssen auf der gleichen elliptischen Kurve liegen.
-    /// TODO: Verdopplung eines Punktes auslagern ala "if equals then call double else add"
+    /// Gibt None zurück, falls die Punkte nicht auf der gleichen Kurve liegen.
     ///
-    fn add(self, other: Self) -> Self {
-        // TODO X: Auf Result umstellen, bezüglich des Asserts
-        assert_eq!(
-            self.curve, other.curve,
-            "Die Punkte müssen auf der gleichen elliptischen Kurve liegen."
-        );
+    pub fn add(&self, other: &Self) -> Option<Self> {
+        if self.curve != other.curve {
+            return None;
+        }
+
+        // Werden identische Punkte addiert, wird die optimierte Verdopplung aufgerufen
+        if self == other {
+            return Some(self.double());
+        }
 
         // Falls einer der beiden Punkte im Ursprung liegt, ist das Ergebnis der andere Punkt
         if self.x.is_zero() && self.y.is_zero() {
-            return other.clone();
+            return Some(other.clone());
         }
         if other.x.is_zero() && other.y.is_zero() {
-            return self.clone();
+            return Some(self.clone());
         }
 
         let p = &self.curve.p;
         let service = NumberTheoryService::new(Fast); // TODO X: Später korrigieren
 
         // Zähler der Steigung berechnen
-        let slope_numer = if self.x == other.x {
-            3u32 * &self.x * &self.x + &self.curve.a
-        } else {
-            &other.y - &self.y
-        };
+        let slope_numer = &other.y - &self.y;
+
         // Nenner der Steigung berechnen
-        let slope_denom = if self.x == other.x {
-            let product = 2u32 * &self.y;
-            service.modulo_inverse(&product, p).unwrap()
-        } else {
-            let diff = &other.x - &self.x;
-            service.modulo_inverse(&diff, p).unwrap()
-        };
+        let slope_denom = service.modulo_inverse(&(&other.x - &self.x), p).unwrap();
+
         // Steigung der Geraden durch die beiden Punkte berechnen
         let slope = (slope_numer * slope_denom).rem_euclid(p);
 
         let x_sum = (&slope * &slope - &self.x - &other.x).rem_euclid(p);
         let y_sum = (&slope * (&self.x - &x_sum) - &self.y).rem_euclid(p);
 
-        EcPoint::new(x_sum, y_sum, self.curve).normalize()
+        Some(EcPoint::new(x_sum, y_sum, Rc::clone(&self.curve)).normalize())
+    }
+
+    ///
+    /// Verdoppelt einen Punkt auf einer elliptischen Kurve.
+    ///
+    pub fn double(&self) -> Self {
+        let p = &self.curve.p;
+        let service = NumberTheoryService::new(Fast); // TODO X: Später korrigieren
+
+        // Zähler der Steigung berechnen
+        let slope_numer = 3u32 * &self.x * &self.x + &self.curve.a;
+        // Nenner der Steigung berechnen
+        let slope_denom = 2u32 * &self.y;
+        let slope_denom = service.modulo_inverse(&slope_denom, p).unwrap();
+        // Steigung der Geraden durch die beiden Punkte berechnen
+        let slope = (slope_numer * slope_denom).rem_euclid(p);
+
+        let x_sum = (&slope * &slope - 2u32 * &self.x).rem_euclid(p);
+        let y_sum = (&slope * (&self.x - &x_sum) - &self.y).rem_euclid(p);
+
+        EcPoint::new(x_sum, y_sum, Rc::clone(&self.curve)).normalize()
     }
 
     ///
@@ -103,53 +119,63 @@ mod tests {
         let curve = get_educational_curve_rc();
         let p1 = EcPoint::new(1.into(), 5.into(), Rc::clone(&curve));
         let p2 = EcPoint::new(5.into(), 9.into(), Rc::clone(&curve));
-        let p3 = p1.add(p2);
-        assert_eq!(p3, EcPoint::new(12.into(), 1.into(), Rc::clone(&curve)));
+        let p3 = p1.add(&p2).unwrap();
+        let expected = EcPoint::new(12.into(), 1.into(), Rc::clone(&curve));
+        assert_eq!(p3, expected);
         let has_point = curve.has_point(&p3);
         assert!(has_point);
 
         let p1 = EcPoint::new(1.into(), 5.into(), Rc::clone(&curve));
         let p2 = EcPoint::new(2.into(), 10.into(), Rc::clone(&curve));
-        let p3 = p1.add(p2);
-        assert_eq!(p3, EcPoint::new(5.into(), 9.into(), Rc::clone(&curve)));
+        let p3 = p1.add(&p2).unwrap();
+        let expected = EcPoint::new(5.into(), 9.into(), Rc::clone(&curve));
+        assert_eq!(p3, expected);
         let has_point = curve.has_point(&p3);
         assert!(has_point);
     }
 
     #[test]
-    #[should_panic]
-    fn test_add_from_different_curves_should_panic() {
+    fn test_add_from_different_curves_gives_none() {
         let curve1 = get_educational_curve_rc();
         let curve2 = Rc::new(EllipticCurve::new((-5).into(), 1.into(), 19.into()));
         let p1 = EcPoint::new(1.into(), 5.into(), Rc::clone(&curve1));
         let p2 = EcPoint::new(5.into(), 9.into(), Rc::clone(&curve2));
-        p1.add(p2);
+        assert_eq!(p1.add(&p2), None);
     }
 
     #[test]
     fn test_add_double_point() {
         let curve = get_educational_curve_rc();
         let p1 = EcPoint::new(1.into(), 5.into(), Rc::clone(&curve));
-        let p2 = p1.clone();
-        let p3 = p1.add(p2);
+        let p2 = p1.add(&p1).unwrap();
+        let p3 = p1.double();
+        // A + A und A.double() sollten das gleiche Ergebnis liefern
+        assert_eq!(p2, p3);
         // 2 * (1, 5) = (2, 10)
-        assert_eq!(p3, EcPoint::new(2.into(), 10.into(), Rc::clone(&curve)));
-        let has_point = curve.has_point(&p3);
+        let expected = EcPoint::new(2.into(), 10.into(), Rc::clone(&curve));
+        assert_eq!(p2, expected);
+        let has_point = curve.has_point(&p2);
         assert!(has_point);
 
         let p1 = EcPoint::new(6.into(), 6.into(), Rc::clone(&curve));
-        let p2 = p1.clone();
-        let p3 = p1.add(p2);
+        let p2 = p1.add(&p1).unwrap();
+        let p3 = p1.double();
+        // A + A und A.double() sollten das gleiche Ergebnis liefern
+        assert_eq!(p2, p3);
         // 2 * (6, 6) = (1, 5)
-        assert_eq!(p3, EcPoint::new(1.into(), 5.into(), Rc::clone(&curve)));
+        let expected = EcPoint::new(1.into(), 5.into(), Rc::clone(&curve));
+        assert_eq!(p3, expected);
         let has_point = curve.has_point(&p3);
         assert!(has_point);
 
         let p1 = EcPoint::new(15.into(), 4.into(), Rc::clone(&curve));
-        let p2 = p1.clone();
-        let p3 = p1.add(p2);
+        let p2 = p1.add(&p1).unwrap();
+        let p3 = p1.double();
+        // A + A und A.double() sollten das gleiche Ergebnis liefern
+        assert_eq!(p2, p3);
         // 2 * (6, 6) = (1, 5)
-        assert_eq!(p3, EcPoint::new(2.into(), 7.into(), Rc::clone(&curve)));
+        let expected = EcPoint::new(2.into(), 7.into(), Rc::clone(&curve));
+        assert_eq!(p3, expected);
         let has_point = curve.has_point(&p3);
         assert!(has_point);
     }
