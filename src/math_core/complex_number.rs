c6204183c5381d6cfe932101c9796b63cb68c98a
Tristan-H11 <triddel02@gmx.de>
2024-04-05 13:36:37 +0200

    Komplexe Zahlen implementiert

---
+++ src/math_core/complex_number.rs
@@ -0,0 +1,62 @@
+use bigdecimal::num_bigint::BigInt;
+use bigdecimal::Signed;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct ComplexNumber {
+    pub real: BigInt,
+    pub imaginary: BigInt,
+}
+
+impl ComplexNumber {
+    pub fn new(real: BigInt, imaginary: BigInt) -> Self {
+        Self {
+            real,
+            imaginary,
+        }
+    }
+
+    pub fn conjugate(&self) -> ComplexNumber {
+        ComplexNumber::new(self.real.clone(), -self.imaginary.clone())
+    }
+
+    pub fn negate(&self) -> ComplexNumber {
+        ComplexNumber::new(-self.real.clone(), -self.imaginary.clone())
+    }
+
+    pub fn add(&self, other: &ComplexNumber) -> ComplexNumber {
+        ComplexNumber::new(
+            &self.real + &other.real,
+            &self.imaginary + &other.imaginary,
+        )
+    }
+
+    pub fn subtract(&self, other: &ComplexNumber) -> ComplexNumber {
+        ComplexNumber::new(
+            &self.real - &other.real,
+            &self.imaginary - &other.imaginary,
+        )
+    }
+
+    pub fn multiply(&self, other: &ComplexNumber) -> ComplexNumber {
+        ComplexNumber::new(
+            &self.real * &other.real - &self.imaginary * &other.imaginary,
+            &self.real * &other.imaginary + &self.imaginary * &other.real,
+        )
+    }
+
+    pub fn divide(&self, other: &ComplexNumber) -> ComplexNumber {
+        let denominator = &other.real * &other.real + &other.imaginary * &other.imaginary;
+        ComplexNumber::new(
+            (&self.real * &other.real + &self.imaginary * &other.imaginary) / &denominator,
+            (&self.imaginary * &other.real - &self.real * &other.imaginary) / &denominator,
+        )
+    }
+
+    pub fn is_in_first_quadrant(&self) -> bool {
+        self.real.is_positive() && self.imaginary.is_positive()
+    }
+
+    pub fn is_in_third_quadrant(&self) -> bool {
+        self.real.is_negative() && self.imaginary.is_negative()
+    }
+}
--- src/math_core/mod.rs
+++ src/math_core/mod.rs
@@ -3,3 +3,4 @@ pub mod ecc;
 pub mod number_theory;
 pub mod pseudo_random_number_generator;
 pub mod traits;
+pub mod complex_number;
