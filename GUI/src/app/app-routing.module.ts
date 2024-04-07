c8a57dd5887529ba5dcca01ffb1cf3bfb7c0218c
lucas <lucas.menke98@gmail.com>
2024-04-07 11:31:51 +0200

    Redesign Frontend: - Remove content from "Startseite" to "RSA" - Remove content from app.component and app.html to startseite.component and startseite.html - Rename of Startseite to Homepage - New Content in Homepage - New Navbar for Routing - Remove Menezes-Vanstone from Navbar List and display it on Homepage with RSA

--- GUI/src/app/app-routing.module.ts
+++ GUI/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {StartseiteComponent} from "./startseite/startseite.component";
+import {HomepageComponent} from "./homepage/homepage.component";
 import {ExponentiationComponent} from "./exponentiation/exponentiation.component";
 import {ExtendedGcdComponent} from "./extendedgcd/extended-gcd.component";
 import {ShanksComponent} from "./shanks/shanks.component";
@@ -8,23 +8,30 @@ import {ClientComponent} from "./client/client.component";
 import {ModularInverseComponent} from "./modular-inverse/modular-inverse.component";
 import {MultiplicationComponent} from "./multiplication/multiplication.component";
 import {MenezesVanstoneComponent} from "./menezes-vanstone/menezes-vanstone.component";
+import {RsaComponent} from "./rsa/rsa.component";
 
+/**
+ * Defines the routes of the application.
+ * The routing takes place in navbar.component.html and homepage.html
+ */
 export const routes: Routes = [
-    {path: "startseite", component: StartseiteComponent},
+    {path: "homepage", component: HomepageComponent},
     {path: "client/:client", component: ClientComponent},
     {path: "menezesVanstone", component: MenezesVanstoneComponent},
+    {path: "rsa", component: RsaComponent},
     {path: "exponentiation", component: ExponentiationComponent},
     {path: "extendedGcd", component: ExtendedGcdComponent},
     {path: "shanks", component: ShanksComponent},
     {path: "modularInverse", component: ModularInverseComponent},
     {path: `multiplication`, component: MultiplicationComponent},
-    {path: "", redirectTo: "/startseite", pathMatch: "full"},
-    {path: "**", component: StartseiteComponent}
+    {path: "", redirectTo: "/homepage", pathMatch: "full"},
+    {path: "**", component: HomepageComponent}
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
+
 export class AppRoutingModule {
 }
--- GUI/src/app/app.config.ts
+++ GUI/src/app/app.config.ts
@@ -5,6 +5,13 @@ import {routes} from './app-routing.module';
 import {provideAnimations} from '@angular/platform-browser/animations';
 import {provideHttpClient} from "@angular/common/http";
 
+/**
+ * Configuration for the application.
+ * providers: List of providers for the application.
+ * provideAnimations: Provider for the animations.
+ * provideHttpClient: Provider for the HttpClient.
+ * provideRouter: Provider for the router.
+ */
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
+    providers: [provideRouter(routes), provideAnimations(), provideHttpClient()]
 };
