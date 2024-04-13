d417c9bd1407717833255fb157332208c823b053
Tristan-H11 <triddel02@gmx.de>
2024-04-13 10:32:40 +0200

    Codestyle

--- GUI/src/app/encryption-components/menezes-vanstone/mv-client-panel/mv-client-panel.component.ts
+++ GUI/src/app/encryption-components/menezes-vanstone/mv-client-panel/mv-client-panel.component.ts
@@ -122,7 +122,6 @@ export class MvClientPanelComponent {
                 this.dialogService.endTimedCalc(loadingCalcKey, "Nachricht verschlüsselt.");
             });
         });
-
     }
 
     /**
