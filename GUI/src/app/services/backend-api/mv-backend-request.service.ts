bd791f1f647f166c2be2f5ee8b6b69d3e8cd0170
tristanhormann <triddel02@gmx.de>
2024-04-19 11:11:11 +0200

    MvRequestService auf Observables umgebaut

--- GUI/src/app/services/backend-api/mv-backend-request.service.ts
+++ GUI/src/app/services/backend-api/mv-backend-request.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@angular/core";
 import {EndpointsService} from "./endpoints.service";
 import {StateManagementService} from "../management/state-management.service";
 import {HttpClient, HttpParams} from "@angular/common/http";
-import {firstValueFrom} from "rxjs";
+import {catchError, EMPTY, Observable} from "rxjs";
 import {SingleMessageModel} from "../../models/SingleMessageModel";
 import {MvKeygenConfig} from "../../models/mv-keygen-config";
 import {
@@ -13,6 +13,7 @@ import {
     MvSignature,
     MvSignRequest, MvVerifyRequest
 } from "../../models/mv-beans";
+import {DialogService} from "../utility/dialogs.service";
 
 @Injectable({
     providedIn: "root"
@@ -24,62 +25,98 @@ export class MvBackendRequestService {
     constructor(
         private endpointsService: EndpointsService,
         private stateService: StateManagementService,
-        private http: HttpClient
+        private http: HttpClient,
+        private dialogService: DialogService
     ) {
     }
 
     /**
      * Fragt den Post Endpunkt zum Erstellen eines neuen Schlüsselpaares ab.
+     * Dabei wird ein Fehler bereits abgefangen und im Dialog angezeigt.
      */
-    public async createKeyPair(body: MvKeygenConfig): Promise<MvKeyPair> {
+    public createKeyPair(body: MvKeygenConfig): Observable<MvKeyPair> {
         const params = this.getParams();
         const options = {params};
-        return firstValueFrom(
-            this.http.post<MvKeyPair>(this.endpointsService.getMvCreateKeyPairEndpoint(), body, options)
+        const response = this.http.post<MvKeyPair>(this.endpointsService.getMvCreateKeyPairEndpoint(), body, options);
+        return response.pipe(
+            catchError(
+                (error) => {
+                    this.dialogService.showErrorDialog(error.error.message)
+                    return EMPTY;
+                }
+            )
         );
     }
 
     /**
      * Fragt den Post Endpunkt zum Verschlüsseln einer Nachricht ab.
+     * Dabei wird ein Fehler bereits abgefangen und im Dialog angezeigt.
      */
-    public async encrypt(body: MvEncryptRequest): Promise<MvCipherText> {
+    public encrypt(body: MvEncryptRequest): Observable<MvCipherText> {
         const params = this.getParams();
         const options = {params};
-        return firstValueFrom(
-            this.http.post<MvCipherText>(this.endpointsService.getMvEncryptEndpoint(), body, options)
+        const response = this.http.post<MvCipherText>(this.endpointsService.getMvEncryptEndpoint(), body, options);
+        return response.pipe(
+            catchError(
+                (error) => {
+                    this.dialogService.showErrorDialog(error.error.message)
+                    return EMPTY;
+                }
+            )
         );
     }
 
     /**
      * Fragt den Post Endpunkt zum Entschlüsseln einer Nachricht ab.
+     * Dabei wird ein Fehler bereits abgefangen und im Dialog angezeigt.
      */
-    public async decrypt(body: MvDecryptRequest): Promise<SingleMessageModel> {
+    public decrypt(body: MvDecryptRequest): Observable<SingleMessageModel> {
         const params = this.getParams();
         const options = {params};
-        return firstValueFrom(
-            this.http.post<SingleMessageModel>(this.endpointsService.getMvDecryptEndpoint(), body, options)
+        const response = this.http.post<SingleMessageModel>(this.endpointsService.getMvDecryptEndpoint(), body, options);
+        return response.pipe(
+            catchError(
+                (error) => {
+                    this.dialogService.showErrorDialog(error.error.message)
+                    return EMPTY;
+                }
+            )
         );
     }
 
     /**
      * Fragt den Post Endpunkt zum Signieren einer Nachricht ab.
+     * Dabei wird ein Fehler bereits abgefangen und im Dialog angezeigt.
      */
-    public async sign(body: MvSignRequest): Promise<MvSignature> {
+    public sign(body: MvSignRequest): Observable<MvSignature> {
         const params = this.getParams();
         const options = {params};
-        return firstValueFrom(
-            this.http.post<MvSignature>(this.endpointsService.getMvSignEndpoint(), body, options)
+        const response = this.http.post<MvSignature>(this.endpointsService.getMvSignEndpoint(), body, options);
+        return response.pipe(
+            catchError(
+                (error) => {
+                    this.dialogService.showErrorDialog(error.error.message)
+                    return EMPTY;
+                }
+            )
         );
     }
 
     /**
      * Fragt den Post Endpunkt zum Verifizieren einer Nachricht ab.
+     * Dabei wird ein Fehler bereits abgefangen und im Dialog angezeigt.
      */
-    public async verify(body: MvVerifyRequest): Promise<SingleMessageModel> {
+    public verify(body: MvVerifyRequest): Observable<SingleMessageModel> {
         const params = this.getParams();
         const options = {params};
-        return firstValueFrom(
-            this.http.post<SingleMessageModel>(this.endpointsService.getMvVerifyEndpoint(), body, options)
+        const response = this.http.post<SingleMessageModel>(this.endpointsService.getMvVerifyEndpoint(), body, options);
+        return response.pipe(
+            catchError(
+                (error) => {
+                    this.dialogService.showErrorDialog(error.error.message)
+                    return EMPTY;
+                }
+            )
         );
     }
 
