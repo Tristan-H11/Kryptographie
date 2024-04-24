621ad261855cf485b315578359c0d1673dbeca10
lucas <lucas.menke98@gmail.com>
2024-04-24 21:55:51 +0200

    Kommentar entfernt

--- GUI/src/app/models/mv-keygen-config.ts
+++ GUI/src/app/models/mv-keygen-config.ts
@@ -1,9 +1,5 @@
 export class MvKeygenConfig { // TODO Mit MvCreateKeyPairRequest zusammenführen
     modulus_width: number;
-    //todo Tristan, wieso wird hier nicht das Number System (Basis) mit angegeben?
-    //in der menezesvanstone.component.ts wird in dem keygen jetzt nichtmehr das hier
-    // verwendet sondern die menezes-vanestone-configuration-data.ts, damit es mit
-    // dem RSA überein stimmt
     miller_rabin_rounds: number;
     coef_a: number;
     random_seed: number;
