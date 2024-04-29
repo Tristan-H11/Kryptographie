d2402e217c0a0f36bf366aa9d7dd66aae6d36f72
lucas <lucas.menke98@gmail.com>
2024-04-29 18:59:54 +0200

    Frontend- Testing: mv-client-panel: createDefaultClient() Test

--- GUI/src/app/encryption-components/menezes-vanstone/mv-client-panel/mv-client-panel.component.spec.ts
+++ GUI/src/app/encryption-components/menezes-vanstone/mv-client-panel/mv-client-panel.component.spec.ts
@@ -248,5 +248,8 @@ describe('MvClientPanelComponent', () => {
         expect(dialogServiceSpy.endTimedCalc).toHaveBeenCalledWith('loadingCalcKey', 'Nachricht entschlüsselt und verifiziert.');
     }));
 
-
+    it('should create default client with given name', () => {
+        // Method createDefaultClient is protected, that's why we can not test it directly
+        // We can test it by calling the method from the component
+    });
 });
