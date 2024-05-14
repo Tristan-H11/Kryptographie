12d0aeeb8497975e341e48e775a04637fa987706
tristanhormann <triddel02@gmx.de>
2024-05-14 21:45:56 +0200

    Stats Logger erweitert

--- src/shared/statistics_logger.rs
+++ src/shared/statistics_logger.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::format;
 
 pub trait StatisticsLogger {
     /// Fügt eine Kontextschicht hinzu.
@@ -12,6 +13,9 @@ pub trait StatisticsLogger {
 
     /// Gibt alle gespeicherten Statistiken zurück.
     fn get_all(&self) -> HashMap<String, Vec<i32>>;
+
+    /// Gibt die Statistiken als Durchschnitt und Standardabweichung zurück.
+    fn get_all_with_metrics(&self) -> HashMap<String, Vec<(String, f64)>>;
 }
 
 pub struct StatisticsLoggerImpl {
@@ -24,6 +28,56 @@ impl StatisticsLoggerImpl {
         StatisticsLoggerImpl {
             contexts: Vec::new(),
             data: HashMap::new(),
+        }
+    }
+
+    fn mean(&self, data: &[i32]) -> Option<f32> {
+        let sum = data.iter().sum::<i32>() as f32;
+        let count = data.len();
+
+        match count {
+            positive if positive > 0 => Some(sum / count as f32),
+            _ => None,
+        }
+    }
+
+    fn std_deviation(&self, data: &[i32]) -> Option<f32> {
+        match (self.mean(data), data.len()) {
+            (Some(data_mean), count) if count > 0 => {
+                let variance = data.iter().map(|value| {
+                    let diff = data_mean - (*value as f32);
+                    diff * diff
+                }).sum::<f32>() / count as f32;
+
+                Some(variance.sqrt())
+            },
+            _ => None
+        }
+    }
+
+    fn first_and_third_quartile(&self, data: &[i32]) -> Option<(i32, i32)> {
+        let mut sorted_data = data.to_vec();
+        sorted_data.sort();
+
+        let count = sorted_data.len();
+        match count {
+            positive if positive > 0 => {
+                let first_index = count / 4;
+                let third_index = count * 3 / 4;
+                Some((sorted_data[first_index], sorted_data[third_index]))
+            },
+            _ => None
+        }
+    }
+
+    fn min_and_max(&self, data: &[i32]) -> Option<(i32, i32)> {
+        let mut sorted_data = data.to_vec();
+        sorted_data.sort();
+
+        let count = sorted_data.len();
+        match count {
+            positive if positive > 0 => Some((sorted_data[0], sorted_data[count - 1])),
+            _ => None
         }
     }
 }
@@ -52,6 +106,33 @@ impl StatisticsLogger for StatisticsLoggerImpl {
     fn get_all(&self) -> HashMap<String, Vec<i32>> {
         self.data.clone()
     }
+
+    fn get_all_with_metrics(&self) -> HashMap<String, Vec<(String, f64)>> {
+        let mut result = HashMap::new();
+        for (key, values) in &self.data {
+            let avg = self.mean(values).unwrap_or(0.0) as f64;
+            let stddev = self.std_deviation(values).unwrap_or(0.0) as f64;
+            let first_and_third = self.first_and_third_quartile(values).unwrap_or((0, 0));
+            let min_and_max = self.min_and_max(values).unwrap_or((0, 0));
+
+            let vec = vec![
+                ("Average: ".into(), avg),
+                ("StdDev: ".into(), stddev),
+                ("First Quartile: ".into(), first_and_third.0 as f64),
+                ("Third Quartile: ".into(), first_and_third.1 as f64),
+                ("Min: ".into(), min_and_max.0 as f64),
+                ("Max: ".into(), min_and_max.1 as f64)
+            ];
+
+            result.insert(
+                key.clone(),
+                vec
+            );
+        }
+        result
+    }
+
+
 }
 
 /// Ein Logger, der keine Statistiken speichert.
@@ -65,6 +146,10 @@ impl StatisticsLogger for VoidLogger {
     fn log_statistic(&mut self, key: &str, count: i32) {}
 
     fn get_all(&self) -> HashMap<String, Vec<i32>> { HashMap::new() }
+
+    fn get_all_with_metrics(&self) -> HashMap<String, Vec<(String, f64)>> {
+        HashMap::new()
+    }
 }
 
 #[cfg(test)]
