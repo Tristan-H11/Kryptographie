ff3066bc9148ac91977f8e611930448aaaca5d24
Tristan Hörmann <45330667+Tristan-H11@users.noreply.github.com>
2024-05-16 19:59:15 +0200

    Update GUI/src/app/app-routing.module.ts

--- GUI/src/app/app-routing.module.ts
+++ GUI/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
-import {CoordinateInputComponent} from "./display-curve/coordinate-input/coordinate-input.component";
 
 export const routes: Routes = [
     {
